59949e81f2f6b274550923f3cc4097335b2830b3
Ekaterina2079 <124146286+Ekaterina2079@users.noreply.github.com>
2023-03-08 13:45:46 +0100

    Create Task5.js

---
+++ Task5.js
@@ -0,0 +1,11 @@
+let arr = [3,4,null,2,0];
+console.log(arr.length)
+
+let result = arr.map(function(item,){
+});
+console.log(arr);
+
+for (let i = 0; i < arr.length; i++) {
+  console.log(`${i} элемент: ${arr[i]}`);
+}  
+              
